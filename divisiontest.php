746a81a60b170206675a390be7bc60a1d65a689f
kencytonn <kmutua@cytonn.com>
2015-09-16 20:06:06 +0300

    PHP testing

---
+++ divisiontest.php
@@ -0,0 +1,30 @@
+<?php
+reuire 'phptest.php'
+$tester= new DivisionTest();
+echo $tester->doTest();
+class DivisionTest{
+	var $division;
+
+	function DivisionTest(){
+	}
+	function doTest(){
+		$this->setUp();
+		$this->testDivision();
+		$this->tearDown();
+	}
+	function setUp(){
+		$this->division=new Division();
+	}
+
+	function tearDown(){
+		unset($this->division);
+	}
+function testDivision(){
+$result=$this->division->divide(10,5);
+if ($result==2){
+	echo "Test Passed";
+}
+else {
+echo "Test Failed";
+}}}
+?>
---
+++ phptest.php
@@ -0,0 +1,11 @@
+<?php
+class Division
+{
+
+function Division(){}
+function divide($x,$y){
+    return $x/$y;
+}
+}
+}
+?>
